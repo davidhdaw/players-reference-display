386c73b215ffa0a01f7d7a80945dc92e82145467
David Daw <daviddaw@gmail.com>
2023-09-23 22:26:22 -0700

    Modal styling, slight refactor to include mode in modal body rather than header to accommodate left border.

--- src/Components/NoteModal/NoteModal.js
+++ src/Components/NoteModal/NoteModal.js
@@ -4,7 +4,7 @@ function NoteModal({note}) {
 
     if (note.type === 'scansion') {
     return(
-        <div className="noteModal">
+        <div className={note.type + 'Modal noteModal'} >
         <h2 className='scansionHeader'>{note.header}</h2>
         <p>{note.pronounciation}</p>
         <p>Rule: {note.rule}</p>
@@ -14,7 +14,7 @@ function NoteModal({note}) {
     )
     } else if (note.type === 'student' || note.type === 'performance') {
         return(
-            <div className="noteModal">
+            <div className={note.type + 'Modal noteModal'} >
             {note.type === 'student' && <h2 className='studentHeader'>{note.header}</h2>}
             {note.type === 'performance' && <h2 className='performanceHeader'>{note.header}</h2>}
             <p>{note.noteContent}</p>
